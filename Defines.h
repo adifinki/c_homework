3ac97a6351a3a9d0424fff9a1f6a710e953f15a2
Adi Finkelstein <61883207+adifinkel@users.noreply.github.com>
2022-04-21 01:50:50 +0300

    Create Defines.h

---
+++ Defines.h
@@ -0,0 +1,44 @@
+#ifndef DEFINES_H
+#define DEFINES_H
+#include <stdio.h>
+// ADD OTHER INCLUDES IF NEEDED.
+
+#define FALSE 0
+#define TRUE 1
+
+#define FIRST_QUESTION 1
+#define SECOND_QUESTION 2
+#define THIRD_QUESTION 3
+
+#define SUCCESS 0
+#define FAIL 1
+
+#define LEGAL_INPUT 1
+#define ILEGAL_INPUT -1
+
+#define DEFAULT 0					//this will be the default value of integer variables for initialization.
+#define EMPTY 0						//THIS IS FOR THE RETURN VALUE OF ZERO IN SCANF.
+
+#define MATRIX_TYPE_ERROR 1
+#define MARIX_SHAPE_ERROR 2
+#define MATRIX_ROWS 3
+#define MATRIX_COULMNS 3
+
+#define MAX_LENGTH 101
+#define DOUBLE_MAX_LENGTH 201
+
+#define DECIMAL_BASE 10
+#define AMOOUNT_OF_ENGLISH_LETTERS 26
+#define DOUBLE_AMOOUNT_OF_ENGLISH_LETTERS  AMOOUNT_OF_ENGLISH_LETTERS*2
+
+#define END_DIVIDER '\n'
+#define ROW_DIVIDER '$'
+#define COL_DIVIDER ' '
+
+#define NEGATIVE '-'
+
+#define MOST_FREQUENT 4
+/*________________________S_____________________________________________________________*/
+
+//ADD HERE OTHER DEFINES IF NEEDED.
+#endif
