3efb9cb0d5aaf370d0586df97c3a58066085e21a
Adi Finkelstein <61883207+adifinki@users.noreply.github.com>
2022-05-27 20:22:50 +0300

    Create Defines.h

---
+++ Defines.h
@@ -0,0 +1,36 @@
+#ifndef DEFINES_H
+#define DEFINES_H
+
+#define FALSE 0
+#define TRUE 1
+#define FAIL 1
+#define SUCCESS 0
+#define GOT_STOP 2
+
+#define STORE_SIZE 50
+#define ITEM_NAME_LENGTH 50
+#define DEPARTMENT_NAME_LENGTH 20
+#define EXPIRATION_DATE_STRING_LENGTH 11
+#define MAX_STRING_LENGTH 50
+#define MAX_ROW_LENGTH 150
+#define STRUCT_FIELDS_NUMBER 5
+
+#define DELIMITER "_*_"
+#define DATE_DELIMITER "."
+#define DATE_VALUES 3
+#define STOP      "STOP\0"
+
+#define NAME 1
+#define DEPARTMENT 2
+#define DATE 3
+#define PRICE 4
+#define AVAILABLE 5
+
+#define STOP_PROGRAM 0
+#define PRINT_PRODUCTS 1
+#define SORT_BY_DATE 2
+#define SORT_BY_PRICE 3
+#define UPDATE 4
+
+
+#endif
