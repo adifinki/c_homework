ce3dd3a217dc59fe492c65cf9ff2a92e700ba3a1
Adi Finkelstein <61883207+adifinki@users.noreply.github.com>
2022-05-28 02:56:01 +0300

    Update Ex4_q2.c

--- Ex4_q2.c
+++ Ex4_q2.c
@@ -7,7 +7,15 @@
 #include "Ex4_q2.h"
 #include "defines.h"
 
-void get_date_from_str(int* date[DATE_VALUES], char* str);
+typedef struct {
+	char* item_name;
+	char* department;
+	char* expiration_date;
+	double price;
+	int available;
+} Item;
+
+void get_date_from_str(int date[DATE_VALUES], char* str);
 int compare_dates(char* first_date, char* second_date);
 int allocate_str_field(char** target, char* source, int max_size);
 int process_item(Item* item, char* item_str);
@@ -22,7 +30,7 @@ void price_sort(Item* store_items, int size);
 void inventory_update(Item** store_items, int* size);
 
 
-void get_date_from_str(int* date[DATE_VALUES], char* str) {
+void get_date_from_str(int date[DATE_VALUES], char* str) {
 	/*
 	* get string date and parse it into array ["day","month","year"]
 	* @param date: target array
@@ -32,39 +40,31 @@ void get_date_from_str(int* date[DATE_VALUES], char* str) {
 	strcpy(copy, str);
 	char* ptr = strtok(copy, DATE_DELIMITER);
 	for (int i = 0; i < DATE_VALUES && ptr != NULL; ++i) {
-		*date[i] = atoi(ptr);
+		date[i] = atoi(ptr);
 		ptr = strtok(NULL, DATE_DELIMITER);
 	}
 }
 
 int compare_dates(char* first_date, char* second_date) {
 	/*
-	* get two date strings and return TRUE if the first date is after the second, 
-	* FALSE if the second date is after the first, SUCCESS if they are identical.
+	* get two date strings and return BIGGER if the first date is after the second, 
+	* SMALLER if the second date is after the first, SUCCESS if they are identical.
 	* @param first_date: string of the first date
 	* @param second_date: string of the second date
 	*/
 	int date1[DATE_VALUES] = { 0 };
 	int date2[DATE_VALUES] = { 0 };
 
-	get_date_from_str(*date1, first_date);
-	get_date_from_str(*date2, second_date);
+	get_date_from_str(date1, first_date);
+	get_date_from_str(date2, second_date);
 
 	for (int i = DATE_VALUES - 1; i >= 0; --i) {
 		if (date1[i] == date2[i])
 			continue;
-		return date1[i] > date2[i] ? TRUE : FALSE;
+		return date1[i] > date2[i] ? BIGGER : SMALLER;
 	}
 	return SUCCESS;
 }
-
-typedef struct {
-	char* item_name;
-	char* department;
-	char* expiration_date;
-	double price;
-	int available;
-}Item;
 
 int allocate_str_field(char** target, char* source, int max_size) {
 	/*
@@ -77,6 +77,8 @@ int allocate_str_field(char** target, char* source, int max_size) {
 	if (max_size < size)
 		return FAIL;
 	*target = (char*)calloc(size+1, sizeof(char));
+	if (target == NULL)
+		return FAIL;
 	memcpy(*target, source, size);
 	return SUCCESS;
 }
@@ -129,7 +131,9 @@ int get_items_str(char items_str[STORE_SIZE][MAX_ROW_LENGTH]) {
 	*/
 	char input[MAX_ROW_LENGTH] = { 0 };
 	for (int i = 0; i < STORE_SIZE; ++i) {
-		fgets(input, MAX_ROW_LENGTH, stdin);
+		char* retval = fgets(input, MAX_ROW_LENGTH, stdin);
+		if (retval == NULL)
+			return FAIL;
 		input[strcspn(input, "\n")] = 0;
 		if (strcmp(input, STOP) == 0) {
 			return i;
@@ -167,7 +171,10 @@ void get_item_str(Item item, char* result) {
 	* @param item: item structer
 	* @param result: pointer to the formatted output
 	*/
-	sprintf(result, "%s"DELIMITER"%s"DELIMITER"%s"DELIMITER"%f"DELIMITER"%d\n", item.item_name, item.department, item.expiration_date, item.price, item.available);
+	int retval = 0;
+	retval =  sprintf(result, "%s"DELIMITER"%s"DELIMITER"%s"DELIMITER"%f"DELIMITER"%d\n", item.item_name, item.department, item.expiration_date, item.price, item.available);
+	if (retval < 0)
+		return FAIL;
 }
 
 void print_inventory(Item* store_items,int size) {
@@ -181,11 +188,10 @@ void print_inventory(Item* store_items,int size) {
 		get_item_str(store_items[i], line);
 		printf("%s", line);
 	}
-	printf("You chose to print the products in the store option.\n\n");
+	printf("\n");
 }
 
 void swap_items(Item* one, Item* two) {
-	//Item* temp = (Item*)malloc(sizeof(temp));
 	/*
 	*swap between two items in a store of items
 	* @param one: struct item
@@ -209,8 +215,6 @@ void swap_items(Item* one, Item* two) {
 	one->expiration_date = temp.expiration_date;
 	one->price = temp.price;
 	one->available = temp.available;
-
-	//free(temp);
 }
 
 
@@ -223,13 +227,11 @@ void expedition_sort(Item* store_items, int size) {
 	for (int i = 0; i < size - 1; ++i) {
 		Item* smallest = &store_items[i];
 		for (int j = i + 1; j < size; ++j) {
-			if (compare_dates(smallest->expiration_date, store_items[j].expiration_date) == TRUE) {
-				printf("%s is bigger then %s\n", smallest->expiration_date, store_items[j].expiration_date);//smallest is greater
+			if (compare_dates(smallest->expiration_date, store_items[j].expiration_date) == BIGGER) {
 				swap_items(smallest, &store_items[j]);
 			}
 		}
 	}
-	printf("You chose to sort the inventory by the expiration date.\n");
 }
 
 void price_sort(Item* store_items, int size) {
@@ -239,15 +241,16 @@ void price_sort(Item* store_items, int size) {
 	* @param size: number of structers
 	*/
 	Item* temp = (Item*)malloc(sizeof(temp));
+	if (temp == NULL)
+		return FAIL;
 	for (int i = 0; i < size; ++i) {
 		for (int j = 0; j < size; ++j) {
-			if (store_items[i].price > store_items[j].price) {
+			if (store_items[i].price < store_items[j].price) {
 				swap_items(&store_items[i], &store_items[j]);
 			}
 		}
 	}
 	free(temp);
-	printf("You chose to sort the inventory by the price.\n");
 }
 
 void inventory_update(Item** store_items, int* size) {
@@ -256,22 +259,26 @@ void inventory_update(Item** store_items, int* size) {
 	* @param store_items: array of item structers to print
 	* @param size: number of structers
 	*/
-	printf("You chose to get rid of all expires items in the inventory.\nPlease enter the present date.\n");
-	char date[EXPIRATION_DATE_STRING_LENGTH] = { 0 };
-	fgets(date, EXPIRATION_DATE_STRING_LENGTH, stdin);
+	char date[MAX_ROW_LENGTH] = { 0 };
+	printf("Please enter the present date.\n");
+	char* retval = fgets(date, MAX_ROW_LENGTH, stdin);
+	if (retval == NULL)
+		return FAIL;
 	date[strcspn(date, "\n")] = 0;
 
 	Item* new_store = (Item*)malloc(STORE_SIZE * sizeof(*new_store));
+	if (new_store == NULL)
+		return FAIL;
 	int count = 0;
 	for (int i = 0; i < *size; ++i)
 	{
-		if (compare_dates((*store_items)[i].expiration_date, date) == FALSE) {
-			free_item(*store_items[i]);
+		if (compare_dates((*store_items)[i].expiration_date, date) == SMALLER) {
+			free_item((*store_items)[i]);
 			continue;
 		}
 
 		char item_str[MAX_ROW_LENGTH];
-		get_item_str(*store_items[i], item_str);
+		get_item_str((*store_items)[i], item_str);
 		process_item(&new_store[count], item_str); 
 		++count;
 	}
@@ -291,13 +298,13 @@ int main(int argc, char* argv[]){
 		"The price in Shekels should be in the formet: xx.yy\n\n"
 		"In order to enyer less than 40 items just enter the string \"STOP\"\n\n"
 	);
-	// char items_str[STORE_SIZE][MAX_ROW_LENGTH];
-	// int size = get_items_str(items_str);
-
-	char items_str[][MAX_ROW_LENGTH] = { "Orens MAGNUM Dark_*_MAGNUM_*_03.05.2022_*_11.90_*_110\0","MAGNUM white Almond_*_MAGNUM_*_06.06.2023_*_11.90_*_38\0","MAGNUM Pint White_*_MAGNUM_*_19.11.2022_*_38.50_*_21\0","MAGNUM Minis_*_MAGNUM_*_23.10.2022_*_27.90_*_40\0","MAGNUM Hazelnum Lux_*_MAGNUM_*_03.05.2022_*_11.90_*_40\0","Danone Yogurt Ice Creame_*_Strauss_*_04.08.2024_*_32.90_*_40\0","Cow Popsicle_*_Strauss_*_15.07.2023_*_08.50_*_18\0","Liklakim_*_Strauss_*_14.08.2022_*_23.50_*_21\0","Ice Cream Car_*_Strauss_*_15.06.2022_*_29.90_*_15\0","Bratz Popsicles_*_Strauss_*_17.03.2023_*_29.90_*_18\0","Yotbeta Popsicles_*_Strauss_*_12.08.2022_*_06.90_*_60\0" };
-	int size = 11, res = SUCCESS, i = 0, choise;
+	char items_str[STORE_SIZE][MAX_ROW_LENGTH], choise[MAX_ROW_LENGTH];
+	int size = get_items_str(items_str), res = SUCCESS, i = 0;
+		
 
 	Item* store_items = (Item*)malloc(STORE_SIZE * sizeof(*store_items));
+	if (store_items == NULL)
+		return FAIL;
 
 	for (; i < size && res == SUCCESS; ++i) {
 		res = process_item(&store_items[i], items_str[i]);
@@ -315,33 +322,40 @@ int main(int argc, char* argv[]){
 				"Choose 4 in order to update the inventory after throwing the expired items.\n"
 				"Choose 0 at any time in order to stop the program and display the final inventory.\n\n"
 			);
-			choise = getchar() - '0'; getchar(); //another one for the \n
 
-			switch (choise) {
+			char* retval = fgets(choise, MAX_ROW_LENGTH, stdin);
+			if (retval == NULL)
+				return FAIL;
+			choise[strcspn(choise, "\n")] = 0;
+
+			switch (atoi(choise)) {
 			case STOP_PROGRAM:
 				printf("Thank you for visiting our store.\nHave a wonderful day.\n");
-				print_inventory(store_items, size);
 				break;
 			case PRINT_PRODUCTS:
+				printf("You chose to print the products in the store option.\n\n");
 				print_inventory(store_items, size);
 				break;
 			case SORT_BY_DATE:
+				printf("You chose to sort the inventory by the expiration date.\n");
 				expedition_sort(store_items, size);
 				print_inventory(store_items, size);
 				break;
 			case SORT_BY_PRICE:
+				printf("You chose to sort the inventory by the price.\n");
 				price_sort(store_items, size);
 				print_inventory(store_items, size);
 				break;
 			case UPDATE:
+				printf("You chose to get rid of all expires items in the inventory.\n");
 				inventory_update(&store_items, &size);
 				print_inventory(store_items, size);
 				break;
 			default:
-				printf("%d bad choise", choise);
+				printf("%s bad choise\n\n", choise);
 				break;
 			}
-		} while (choise != STOP_PROGRAM);
+		} while (atoi(choise) != STOP_PROGRAM);
 	}
 
 
